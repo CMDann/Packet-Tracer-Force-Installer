759025c9d9fe05aa1026f5bfb0e1ddcb821f8f85
Daniel Blair <CMDann.Development@gmail.com>
2013-09-09 22:17:07 -0500

    Create PacketTracerForceInstaller.sh
    Adding the initial script

---
+++ PacketTracerForceInstaller.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# PacketTracer Installer
+# Written by K. Law @ http://kaslit.com
+# Modified by D. Blair http://www.CMDann.ca
+#
+# PcketTracer is software by Cisco
+#
+# This script will aid in the installation of the cisco packet tracer
+
+echo "Installing all required Libraries"
+apt-get -y install ia32-libs-gtk
+
+echo "Moving files and doing sciency stuff "
+cp -v /tmp/selfextract.*/Packet* ./
+
+echo "Successful"
+echo " "
+echo "Installing Package Libraries"
+echo " "
+echo "Running the important files"
+dpkg -i --force-architecture Packet*.deb
+
+echo " "
+echo "PacketTracer is installed!"
+echo " "
+echo "Applying system libs"
+getlibs /usr/local/PacketTracer5/bin/PacketTracer5
+
+echo " "
+echo "PacketTracer installation should be complete"
+echo "This script does not detect errors, if for some reason the installer dosen't install correctly try downloading a new bin from the Cisco network academy website."
+echo " "
+echo "Running PacketTracer"
+echo " "
+echo "If PacketTracer didn't install look for the ia32-libs-gtk file in the Debian/Ubuntu repositories. This file is the important one for a successful install." > ~/PacketTracer_README_IF_IT_FAILED.txt
+/usr/local/PacketTracer5/packettracer
